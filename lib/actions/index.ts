caa307c9b479ffca13860d2058c8b1e59e795a83
Yasir Khan <khanyaser007@gmail.com>
2023-10-15 20:01:52 +0800

    Add server-side logic for retrieving all products and similar

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -2,19 +2,18 @@
 
 import { revalidatePath } from "next/cache";
 import Product from "../models/product.model";
-import { connetToDB } from "../mongoose";
+import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
 import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
 
-export async function scrapeAndStoreProduct(productUrl:string) {
+export async function scrapeAndStoreProduct(productUrl: string) {
   if(!productUrl) return;
 
   try {
-
-    connetToDB();
+    connectToDB();
 
     const scrapedProduct = await scrapeAmazonProduct(productUrl);
-    
+
     if(!scrapedProduct) return;
 
     let product = scrapedProduct;
@@ -37,27 +36,57 @@ export async function scrapeAndStoreProduct(productUrl:string) {
     }
 
     const newProduct = await Product.findOneAndUpdate(
-      { url: scrapedProduct.url},
+      { url: scrapedProduct.url },
       product,
       { upsert: true, new: true }
     );
 
     revalidatePath(`/products/${newProduct._id}`);
   } catch (error: any) {
-    throw new Error(`Falied to create/update the product: ${error.message}`)
+    throw new Error(`Failed to create/update product: ${error.message}`)
   }
 }
 
 export async function getProductById(productId: string) {
   try {
-    connetToDB();
+    connectToDB();
 
-    const product = await Product.findOne({_id: productId});
+    const product = await Product.findOne({ _id: productId });
 
     if(!product) return null;
 
     return product;
   } catch (error) {
-    console.log(error)
+    console.log(error);
+  }
+}
+
+export async function getAllProducts() {
+  try {
+    connectToDB();
+
+    const products = await Product.find();
+
+    return products;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getSimilarProducts(productId: string) {
+  try {
+    connectToDB();
+
+    const currentProduct = await Product.findById(productId);
+
+    if(!currentProduct) return null;
+
+    const similarProducts = await Product.find({
+      _id: { $ne: productId },
+    }).limit(3);
+
+    return similarProducts;
+  } catch (error) {
+    console.log(error);
   }
 }
